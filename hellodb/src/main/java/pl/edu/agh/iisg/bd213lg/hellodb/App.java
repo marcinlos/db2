13697a9919d49e07f8aa021304a9595040891e92
los <losiu99@gazeta.pl>
2013-04-14 21:06:37 +0200

    It's working

--- hellodb/src/main/java/pl/edu/agh/iisg/bd213lg/hellodb/App.java
+++ hellodb/src/main/java/pl/edu/agh/iisg/bd213lg/hellodb/App.java
@@ -1,16 +1,7 @@
 package pl.edu.agh.iisg.bd213lg.hellodb;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import javax.sql.DataSource;
-
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.jdbc.CannotGetJdbcConnectionException;
-import org.springframework.jdbc.datasource.DataSourceUtils;
 
 public class App {
     public static void main(String[] args) {
