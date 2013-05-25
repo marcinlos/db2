9343be7fedba25bf957b172700c246d1566a0af2
los <losiu99@gazeta.pl>
2013-05-25 21:17:28 +0200

    Added "base" version of first bootcamp

---
+++ base/src/main/java/pl/edu/agh/iisg/bd213lg/hellodb/App.java
@@ -0,0 +1,15 @@
+package pl.edu.agh.iisg.bd213lg.hellodb;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        try (ConfigurableApplicationContext context = 
+                new ClassPathXmlApplicationContext("Spring-Module.xml")) {
+            HelloDatabase obj = (HelloDatabase) context.getBean("helloBean");
+	    obj.sayHello();
+            
+        }
+    }
+}
---
+++ base/src/main/java/pl/edu/agh/iisg/bd213lg/hellodb/dao/CustomerDAO.java
@@ -0,0 +1,21 @@
+package pl.edu.agh.iisg.bd213lg.hellodb.dao;
+
+import java.util.Collection;
+
+import org.hibernate.criterion.Criterion;
+
+import pl.edu.agh.iisg.bd213lg.hellodb.domain.Customer;
+
+public interface CustomerDAO {
+    
+	Customer find(String id);
+    
+    Collection<Customer> findAll();
+    
+    boolean save(Customer entity);
+    
+    boolean delete (Customer entity);
+    
+    public boolean update(Customer entity);
+
+}
